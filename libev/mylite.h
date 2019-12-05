992eb828cdaf99a6cdb6145d4c03284f14b234ae
zfl9 <zfl9.com@gmail.com>
2019-12-05 16:41:37 +0800

    Embed libev library

--- libev/mylite.h
+++ libev/mylite.h
@@ -4,8 +4,9 @@
 /* libev-4.27 */
 #define EV_STANDALONE 1 /* manual configuration */
 #define EV_COMPAT3 0 /* remove compatible code */
-#define EV_USE_FLOOR 1 /* use libm.floor() func */
-#define EV_NO_THREADS 1 /* never be called by other threads */
+#define EV_VERIFY 0 /* remove verification code */
+#define EV_USE_FLOOR 1 /* use libm.floor() function */
+#define EV_NO_THREADS 1 /* disable multi-threads support */
 #define EV_PERIODIC_ENABLE 0 /* disable ev_periodic watcher */
 #define EV_IDLE_ENABLE 0 /* disable ev_idle watcher */
 #define EV_EMBED_ENABLE 0 /* disable ev_embed watcher */
@@ -17,6 +18,5 @@
 #define EV_ASYNC_ENABLE 0 /* disbale ev_async watcher */
 #define EV_CHILD_ENABLE 0 /* disable ev_child watcher */
 #define EV_CLEANUP_ENABLE 0 /* disable ev_cleanup watcher */
-#define EV_VERIFY 0 /* remove verification code */
 
 #endif
