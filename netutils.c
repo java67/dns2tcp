7121fd452b5115a1e7b12291f7a2450b91b31877
zfl9 <zfl9.com@gmail.com>
2019-12-05 20:58:33 +0800

    Refactoring the netutils module

--- netutils.c
+++ netutils.c
@@ -1,33 +1,15 @@
 #define _GNU_SOURCE
 #include "netutils.h"
+#include "logutils.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>
 #undef _GNU_SOURCE
-
-/* build ipv4 socket address from ipstr and portno */
-void build_ipv4_addr(skaddr4_t *addr, const char *ipstr, portno_t portno) {
-    addr->sin_family = AF_INET;
-    inet_pton(AF_INET, ipstr, &addr->sin_addr);
-    addr->sin_port = htons(portno);
-}
-
-/* build ipv6 socket address from ipstr and portno */
-void build_ipv6_addr(skaddr6_t *addr, const char *ipstr, portno_t portno) {
-    addr->sin6_family = AF_INET6;
-    inet_pton(AF_INET6, ipstr, &addr->sin6_addr);
-    addr->sin6_port = htons(portno);
-}
-
-/* parse ipstr and portno from ipv4 socket address */
-void parse_ipv4_addr(const skaddr4_t *addr, char *ipstr, portno_t *portno) {
-    inet_ntop(AF_INET, &addr->sin_addr, ipstr, IP4STRLEN);
-    *portno = ntohs(addr->sin_port);
-}
-
-/* parse ipstr and portno from ipv6 socket address */
-void parse_ipv6_addr(const skaddr6_t *addr, char *ipstr, portno_t *portno) {
-    inet_ntop(AF_INET6, &addr->sin6_addr, ipstr, IP6STRLEN);
-    *portno = ntohs(addr->sin6_port);
-}
 
 /* AF_INET or AF_INET6 or -1(invalid ip string) */
 int get_ipstr_family(const char *ipstr) {
@@ -39,5 +21,59 @@ int get_ipstr_family(const char *ipstr) {
         return AF_INET6;
     } else {
         return -1;
+    }
+}
+
+/* before calling, please memset(skaddr) to 0 */
+void build_skaddr4_from_ipport(skaddr4_t *skaddr, const char *ipstr, portno_t portno) {
+    skaddr->sin_family = AF_INET;
+    inet_pton(AF_INET, ipstr, &skaddr->sin_addr);
+    skaddr->sin_port = htons(portno);
+}
+void build_skaddr6_from_ipport(skaddr6_t *skaddr, const char *ipstr, portno_t portno) {
+    skaddr->sin6_family = AF_INET6;
+    inet_pton(AF_INET6, ipstr, &skaddr->sin6_addr);
+    skaddr->sin6_port = htons(portno);
+}
+
+void parse_ipport_from_skaddr4(const skaddr4_t *skaddr, char *ipstr, portno_t *portno) {
+    inet_ntop(AF_INET, &skaddr->sin_addr, ipstr, IP4STRLEN);
+    *portno = ntohs(skaddr->sin_port);
+}
+void parse_ippport_from_skaddr6(const skaddr6_t *skaddr, char *ipstr, portno_t *portno) {
+    inet_ntop(AF_INET6, &skaddr->sin6_addr, ipstr, IP6STRLEN);
+    *portno = ntohs(skaddr->sin6_port);
+}
+
+void make_socket_nonblocking(int sockfd) {
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags == -1) {
+        LOGERR("[make_socket_nonblocking] fcntl(%d, F_GETFL): (%d) %s", sockfd, errno, strerror(errno));
+        exit(errno);
+    }
+    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        LOGERR("[make_socket_nonblocking] fctnl(%d, F_SETFL): (%d) %s", sockfd, errno, strerror(errno));
+        exit(errno);
+    }
+}
+
+void set_socketopt_ipv6_only(int sockfd) {
+    if (setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &(int){1}, sizeof(int)) == -1) {
+        LOGERR("[set_socketopt_ipv6_only] setsockopt(%d, IPV6_V6ONLY): (%d) %s", sockfd, errno, strerror(errno));
+        exit(errno);
+    }
+}
+
+void set_socketopt_reuse_addr(int sockfd) {
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1) {
+        LOGERR("[set_socketopt_reuse_addr] setsockopt(%d, SO_REUSEADDR): (%d) %s", sockfd, errno, strerror(errno));
+        exit(errno);
+    }
+}
+
+void set_socketopt_tcp_nodelay(int sockfd) {
+    if (setsockopt(sockfd, SOL_TCP, TCP_NODELAY, &(int){1}, sizeof(int)) == -1) {
+        LOGERR("[set_socketopt_tcp_nodelay] setsockopt(%d, TCP_NODELAY): (%d) %s", sockfd, errno, strerror(errno));
+        exit(errno);
     }
 }
--- netutils.h
+++ netutils.h
@@ -7,36 +7,32 @@
 #undef _GNU_SOURCE
 
 /* mtu(1500) - iphdr(20) - udphdr(8) */
-#define DNS_PACKET_MAXSIZE 1472 /* bytes */
+#define UDP_PACKET_MAXSIZE 1472 /* bytes */
 
-/* ipaddr string len */
 #define IP4STRLEN INET_ADDRSTRLEN
 #define IP6STRLEN INET6_ADDRSTRLEN
 
-/* portno string len */
 #define PORTSTRLEN 6
 
-/* port number typedef */
 typedef uint16_t portno_t;
 
-/* sockaddr type alias */
 typedef struct sockaddr     skaddr_t;
 typedef struct sockaddr_in  skaddr4_t;
 typedef struct sockaddr_in6 skaddr6_t;
 
-/* build ipv4 socket address from ipstr and portno */
-void build_ipv4_addr(skaddr4_t *addr, const char *ipstr, portno_t portno);
-
-/* build ipv6 socket address from ipstr and portno */
-void build_ipv6_addr(skaddr6_t *addr, const char *ipstr, portno_t portno);
-
-/* parse ipstr and portno from ipv4 socket address */
-void parse_ipv4_addr(const skaddr4_t *addr, char *ipstr, portno_t *portno);
-
-/* parse ipstr and portno from ipv6 socket address */
-void parse_ipv6_addr(const skaddr6_t *addr, char *ipstr, portno_t *portno);
-
-/* AF_INET or AF_INET6 or -1(invalid ip string) */
+/* AF_INET or AF_INET6 or -1(invalid) */
 int get_ipstr_family(const char *ipstr);
+
+/* before calling, please memset(skaddr) to 0 */
+void build_skaddr4_from_ipport(skaddr4_t *skaddr, const char *ipstr, portno_t portno);
+void build_skaddr6_from_ipport(skaddr6_t *skaddr, const char *ipstr, portno_t portno);
+
+void parse_ipport_from_skaddr4(const skaddr4_t *skaddr, char *ipstr, portno_t *portno);
+void parse_ipport_from_skaddr6(const skaddr6_t *skaddr, char *ipstr, portno_t *portno);
+
+void make_socket_nonblocking(int sockfd);
+void set_socketopt_ipv6_only(int sockfd);
+void set_socketopt_reuse_addr(int sockfd);
+void set_socketopt_tcp_nodelay(int sockfd);
 
 #endif
